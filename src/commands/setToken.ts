a083be9c6f26c431bc8c70887a81c82121329704
feri-irawan <feriirawan.akun@gmail.com>
2025-07-08 15:20:55 +0800

    feat(command): add set token command

---
+++ src/commands/setToken.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+import { getContext } from '../lib/context';
+
+const TOKEN_KEY = 'ficommitgen.token';
+
+export const showInputToken = async () => {
+  const token = await vscode.window.showInputBox({
+    prompt: 'Masukkan token API Gemini kamu',
+    ignoreFocusOut: true,
+    password: true,
+    placeHolder: 'Contoh: AIzaSy...',
+    validateInput: (value) => (value.trim().length === 0 ? 'Token tidak boleh kosong' : undefined),
+  });
+
+  if (!token) {
+    vscode.window.showWarningMessage('⚠️ Token tidak dimasukkan.');
+    return;
+  }
+
+  return token;
+};
+
+export const registerSetTokenCommand = (context: vscode.ExtensionContext) => {
+  const disposable = vscode.commands.registerCommand('ficommitgen-vscode.setToken', async () => {
+    const context = getContext();
+
+    const newToken = await showInputToken();
+    if (!newToken) {
+      return;
+    }
+
+    await context.secrets.store(TOKEN_KEY, newToken);
+    vscode.window.showInformationMessage('✅ Token berhasil disimpan (ditimpa jika sebelumnya sudah ada).');
+  });
+
+  context.subscriptions.push(disposable);
+};
--- src/lib/getOrRequestToken.ts
+++ src/lib/getOrRequestToken.ts
@@ -1,29 +1,22 @@
 import * as vscode from 'vscode';
 import { getContext } from './context';
+import { showInputToken } from '../commands/setToken';
 
 const TOKEN_KEY = 'ficommitgen.token';
 
 const getOrRequestToken = async (): Promise<string | undefined> => {
   const context = getContext();
 
-  const secretStorage = context.secrets;
-  let token = await secretStorage.get(TOKEN_KEY);
+  let token = await context.secrets.get(TOKEN_KEY);
 
   if (!token) {
-    token = await vscode.window.showInputBox({
-      prompt: 'Masukkan token API Gemini kamu',
-      ignoreFocusOut: true,
-      password: true,
-      placeHolder: 'Contoh: AIzaSy...',
-      validateInput: (value) => (value.trim().length === 0 ? 'Token tidak boleh kosong' : undefined),
-    });
-
-    if (token) {
-      await secretStorage.store(TOKEN_KEY, token);
-      vscode.window.showInformationMessage('✅ Token berhasil disimpan!');
-    } else {
-      vscode.window.showWarningMessage('⚠️ Token tidak dimasukkan. Beberapa fitur mungkin tidak bisa digunakan.');
+    token = await showInputToken();
+    if (!token) {
+      return;
     }
+
+    await context.secrets.store(TOKEN_KEY, token);
+    vscode.window.showInformationMessage('✅ Token berhasil disimpan!');
   }
 
   return token;
